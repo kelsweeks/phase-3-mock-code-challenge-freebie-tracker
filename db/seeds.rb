c0c0532203dbc020250bfe7d6416fe2a09173408
Kelsey Weeks <kelsey@kelseyweeks.com>
2022-08-24 08:39:26 -0500

    commit tree 00abb91d08db0aef7f77f4caad8850f47caad76c

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
@@ -11,6 +16,17 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+
+# items = [ 'lambo', 'rolex', 'sticker', 'frisbee' ]
+
+# 7.times do
+#     Freebie.create(
+#         dev_id: Dev.pluck( :id ).sample,
+#         company: Company.all.sample,
+#         item_name: items.sample,
+#         value: rand( 7..100000 )
+#     )
+end
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
